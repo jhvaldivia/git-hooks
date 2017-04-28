228f6ed541e2bc7a320136f9985f799b51556fde
Jaime Valdivia <jaivaldivia@gmail.com>
2017-04-27 20:02:46 -0500

    Initial commit

---
+++ src/main/java/com/draco/DracoApplication.java
@@ -0,0 +1,12 @@
+package com.draco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DracoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DracoApplication.class, args);
+	}
+}
